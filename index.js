71a10707be081d045d0b53c4e942323a58e2f877
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-03-12 21:13:48 +0300

    js file assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = " Harriet Chepng\'eno"
+const welcomeMessage = "Welcome to Flatbook, Harriet Chepng\'eno!";
+const excitedWelcomeMessage = " WELCOME TO FLATBOOK, HARRIET CHEPNG\'ENO!";
+const shortGreeting = "Welcome, Harriet!";
